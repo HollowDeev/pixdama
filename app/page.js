3799e2a7cfafdf8e9e05f528ca885f8534bd36f5
Thay <thaua19felipe@gmail.com>
2024-07-12 17:35:48 -0300

    update: modificando borda da foto de tabuleiro

--- app/page.js
+++ app/page.js
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center  px-24 py-9">
       <div className="w-full flex h-[500px]">
-        <img src="tabuleiro.jpg" width="92%" height="300px" />
+        <img src="tabuleiro.jpg" width="92%" height="300px" className="rounded-tl-3xl rounded-bl-3xl"/>
         <div className=" w-20 bg-white text-5xl font-black rounded-tr-3xl rounded-br-3xl flex flex-col items-center justify-center cursor-pointer hover:bg-stone-900 hover:text-white transition-all">
           <p>J</p>
           <p>O</p>
