ee327ab71bbd6f5d78540e239e48deb91a0973c2
Thay <thaua19felipe@gmail.com>
2024-07-23 10:01:31 -0300

    feat: sistema de login basico

---
+++ app/actions/login.js
@@ -0,0 +1,34 @@
+'use client'
+
+import { createClient } from "@/utils/supabase/client"
+
+export async function login(email, senha) {
+    const supabase = createClient()
+    let sucessoLogin = false
+    let resposta = {}
+    
+    const data = {
+      email: email,
+      password: senha
+    }
+
+  
+    const { error } = await supabase.auth.signInWithPassword(data)
+  
+    if (error) {
+      sucessoLogin = false 
+      resposta = {
+        sucessoLogin,
+        mensagem: error
+      }
+    } else {
+      sucessoLogin = true 
+      resposta = {
+        sucessoLogin,
+        mensagem: 'Login Realizado com sucesso'
+      }
+    }
+
+    return resposta
+    
+}
